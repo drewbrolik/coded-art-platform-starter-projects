03a2da9a42864ced7cf5a61114dcc597cb730660
Drew Thomas <drew@truedrew.co>
2024-06-30 16:48:47 -0500

    highlight p5.js - initial files

---
+++ highlight-p5js/README.md
@@ -0,0 +1,16 @@
+# Highlight Generative Collection Starter Project
+
+## with p5.js
+
+---
+
+The easiest way to download these files is to download the whole repo and then pull out this folder. To download the full repo, use the dropdown on the green Code button on the repo homepage: [https://github.com/drewbrolik/coded-art-starter-kits](https://github.com/drewbrolik/coded-art-starter-kits).
+
+### Highlight docs
+[https://github.com/highlightxyz/generative-art](https://github.com/highlightxyz/generative-art)
+
+### TL;DR
+Use these files to start a Highlight.xyz generative art project, using the p5.js library. After adding your code to the script.js file, upload these files directly to Highlight.
+
+### Remove this file
+This file, README.md, is not necessary as part of the package uploaded to Highlight. You can delete it, and it won't affect your project.
---
+++ highlight-p5js/script.js
@@ -0,0 +1,72 @@
+/* **** **** ****
+* declare global variables
+* highlight docs:
+* https://github.com/highlightxyz/generative-art
+**** **** **** */
+const hash = hl.tx.hash;
+const tokenId = hl.tx.tokenId;
+
+// for predictable randomness example only
+let exampleRandomValue;
+
+
+
+
+
+/* **** **** ****
+* p5.js functions
+**** **** **** */
+function setup() {
+
+  // fxhash predictable randomness
+  $fx.rand.reset();
+  exampleRandomValue = hl.random(0, 1);
+
+  // Traits, title, and description
+  let traits = {};
+  traits["Trait Name 1"] = "Trait Value 1";
+  traits["Trait Name 2"] = "Trait Value 2";
+
+  //console.log(traits);
+  hl.token.setTraits(traits);
+
+  // Also set a name and description for this token
+  hl.token.setName(`Collection Name #${hl.tx.tokenId}`);
+  hl.token.setDescription(
+      ``
+  );
+
+}
+
+function draw() {
+
+  // tell fxhash to capture preview image here
+  if (hl.context.previewMode) { hl.token.capturePreview(); }
+
+  //noLoop();
+
+}
+
+function keyTyped() {
+  
+  if (key === 's') {
+    save(tokenId+'.png');
+  }
+
+}
+
+function windowResized() {
+  
+}
+
+
+
+
+
+
+/* **** **** ****
+* non-p5.js functions
+**** **** **** */
+function doSomethingCustom() {
+
+}
